44071e50e60130ccf68fbc9a420baf744dd53edc
ziruiqiao <qiao.zir@northeastern.edu>
2024-07-26 13:59:34 -0400

    user icon drop down list

--- src/components/main/userIcon/UserIcon.js
+++ src/components/main/userIcon/UserIcon.js
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 import style from './UserIcon.module.css';
+import { useAuth } from "components/guards/AuthContext";
 
-const UserIcon = () => (
-    <div className={style.wrapper}>
-        <FontAwesomeIcon icon={faUser} className={style.icon} />
-    </div>
-);
+const UserIcon = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const { signOut } = useAuth();
+    const dropdownRef = useRef(null);
+
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const handleClickOutside = (event) => {
+        if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+            setIsOpen(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
+    return (
+        <div className={style.wrapper} ref={dropdownRef} onClick={toggleDropdown} >
+            <button className={style.iconButton}>
+                <FontAwesomeIcon icon={faUser} className={style.icon} />
+            </button>
+            {isOpen && (
+                <div className={style.dropdownMenu}>
+                    <ul className={style.dropdownList}>
+                        <li className={style.dropdownItem}>Profile</li>
+                        <li className={style.dropdownItem}>Settings</li>
+                        <li 
+                            className={style.dropdownItem} 
+                            onClick={signOut}
+                        >Logout</li>
+                    </ul>
+                </div>
+            )}
+        </div>
+    );
+};
 
 export default UserIcon;
