ad0e1a26d492d8247d0a1368467c09b3cae73c22
elarriaga <eliseo.arriaga@flomad.com>
2024-08-01 15:16:36 -0700

    Title correction

--- src/components/landingPage/landing/scrollableRestaurants/ScrollableRestaurant.js
+++ src/components/landingPage/landing/scrollableRestaurants/ScrollableRestaurant.js
@@ -18,7 +18,7 @@ const ScrollableDestination = () => {
   return (
     <section id="scrollable-destination" className={styles['scrollable-destination']}>
       <div className={styles.container}>
-        <h2 className={styles.heading}>Popular Travel Destinations</h2>
+        <h2 className={styles.heading}>Gourmet Destinations</h2>
         <div className={styles['image-container']}>
           {destinations.map((destination, index) => (
             <div
