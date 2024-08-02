1e1cc2b96cf0ca8e8ca084db7a5ade6d97025638
elarriaga <eliseo.arriaga@flomad.com>
2024-08-02 15:16:30 -0700

    Adjusted the landingpage, to fit different screen aspect ratios

--- src/components/landingPage/landing/contact/Contact.js
+++ src/components/landingPage/landing/contact/Contact.js
@@ -4,12 +4,12 @@ import styles from './Contact.module.css';
 const Contact = () => (
   <section id="contact" className={`${styles.contact} ${styles['flex-container']}`}>
     <div className={styles.container}>
-      <h2 className="original-h2" style={{ fontSize: '24px' }}>Contact Us</h2>
-      <p style={{ fontSize: '18px' }}>Have questions or need support? Reach out to us!</p>
+      <h2 className="original-h2">Contact Us</h2>
+      <p>Have questions or need support? Reach out to us!</p>
       <form action="#" method="post">
-        <input type="email" name="email" placeholder="Your Email" style={{ fontSize: '18px' }} required />
-        <textarea name="message" placeholder="Your Message" style={{ fontSize: '18px' }} required></textarea>
-        <button type="submit" className={styles.cta} style={{ fontSize: '18px' }}>Send Message</button>
+        <input type="email" name="email" placeholder="Your Email" required />
+        <textarea name="message" placeholder="Your Message" required></textarea>
+        <button type="submit" className={styles.cta}>Send Message</button>
       </form>
     </div>
   </section>
