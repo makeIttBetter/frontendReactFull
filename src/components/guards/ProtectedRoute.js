f7b03098fb97ea3f7a1b2741c41485f5d0f484d4
elarriaga <eliseo.arriaga@flomad.com>
2024-08-14 18:20:22 -0700

    Update

--- src/components/guards/ProtectedRoute.js
+++ src/components/guards/ProtectedRoute.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from './AuthContext';
 
-const ProtectedRoute = ({ children, requireAuth = true }) => {
+const ProtectedRoute = ({ children }) => {
   const { isAuthenticated } = useAuth();
 
   // If authentication is required and the user is not authenticated, redirect to /auth
-  if (requireAuth && !isAuthenticated) {
+  if (!isAuthenticated) {
     return <Navigate to="/auth" />;
   }
 
