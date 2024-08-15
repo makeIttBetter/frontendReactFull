1cab4beb6f84d888781d0871bd16f8a36ce3746e
elarriaga <eliseo.arriaga@flomad.com>
2024-08-14 18:11:26 -0700

    Resizing the mobile website Chat interface

--- src/components/guards/ProtectedRoute.js
+++ src/components/guards/ProtectedRoute.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from './AuthContext';
 
-const ProtectedRoute = ({ children }) => {
+const ProtectedRoute = ({ children, requireAuth = true }) => {
   const { isAuthenticated } = useAuth();
 
-  if (!isAuthenticated) {
+  // If authentication is required and the user is not authenticated, redirect to /auth
+  if (requireAuth && !isAuthenticated) {
     return <Navigate to="/auth" />;
   }
 
+  // Otherwise, render the children components
   return children;
 };
 
