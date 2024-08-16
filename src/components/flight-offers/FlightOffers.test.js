ac5a53f47c8b7430904674abf7eb36d993030e30
Jonathan Zhao <jonathanzhao64@gmail.com>
2024-08-16 13:22:45 -0500

    Revert "Flight Offers"
    This reverts commit ac6930e44eea5c0a2096730f2d7f1c4fabede46b.

--- src/components/flight-offers/FlightOffers.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Reference from './FlightOffers';
-
-test('renders learn react link', () => {
-  render(<Reference />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
--- src/components/reference/Reference.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Reference from './reference';
-
-test('renders learn react link', () => {
-  render(<Reference />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
